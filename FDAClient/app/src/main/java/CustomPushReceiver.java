093eb128b11240b494cd7b66a19f971e96dcda9b
vikas pydah <vikas pydah>
2016-04-20 10:11:34 -0500

    Push Notifications

---
+++ FDAClient/app/src/main/java/CustomPushReceiver.java
@@ -0,0 +1,24 @@
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.parse.ParsePushBroadcastReceiver;
+
+/**
+ * Created by Vikingprime on 4/20/2016.
+ */
+public class CustomPushReceiver extends ParsePushBroadcastReceiver {
+
+    public CustomPushReceiver() {
+        super();
+    }
+
+    @Override
+    protected void onPushReceive(Context context, Intent intent) {
+
+        Log.d("TAG","PUSH RECEIVED");
+        super.onPushReceive(context, intent);
+        Log.d("TAG","PUSH RECEIVED");
+
+    }
+}
--- FDAClient/app/src/main/java/com/example/android/fdaclient/MainApplication.java
+++ FDAClient/app/src/main/java/com/example/android/fdaclient/MainApplication.java
@@ -1,17 +1,50 @@
 package com.example.android.fdaclient;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 /**
  * Created by sanji on 4/19/2016.
  */
 public class MainApplication extends Application {
+    private static MainApplication sInstance;
+
+    /**
+     * @return ApplicationController singleton instance
+     */
+    public static synchronized MainApplication getInstance() {
+        return sInstance;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d("TAG","In application onCreate");
+        Parse.initialize(this, getResources().getString(R.string.applicationid),
+                getResources().getString(R.string.clientkey));
+
+
+//        ParsePush.subscribeInBackground("", new SaveCallback() {
+//            @Override
+//            public void done(ParseException e) {
+//                if (e == null) {
+//
+//                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+//                } else {
+//                    Log.e("com.parse.push", "failed to subscribe for push", e);
+//                }
+//            }
+//        });
+
         ParseInstallation.getCurrentInstallation().saveInBackground();
+        // initialize the singleton
+        sInstance = this;
+
     }
 }
